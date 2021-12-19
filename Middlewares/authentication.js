18279b7ed2e027c9f772381b6067ec0fd55b9a78
Hassan Ali <7assan.3li1998@gmail.com>
2021-12-19 11:33:18 +0200

    auth issue #2

--- Middlewares/authentication.js
+++ Middlewares/authentication.js
@@ -4,7 +4,9 @@ const authenticated = (req, res, next) => {
     try {
         const token = req.cookies.portfolioCurrentAdmin;
 
-        console.log(token);
+        console.log(req.cookies);
+
+        console.log("Token: " + token);
 
         if (!token) return res.send({err: "Authorization Required"});
 
